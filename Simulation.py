713a4e4768a9cb439bfa7404b931d4e3471eee78
Alex Wheaton <34541330+aswheaton@users.noreply.github.com>
2018-03-29 17:41:12 +0100

    Create Simulation.py

---
+++ Simulation.py
@@ -0,0 +1,11 @@
+"""
+    N-body simulation class object.
+    Author: Alexander S. Wheaton
+    Date: 29 March 2018
+    Updated: 29 March January 2018
+"""
+
+class Simulation(object):
+    
+    # Recieves a 
+    def __init__(self, filename):
