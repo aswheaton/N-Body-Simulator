95b75c3f89154bb64a3969de41224fb34d400dcf
Alex Wheaton <34541330+aswheaton@users.noreply.github.com>
2018-04-04 19:49:56 +0100

    Implemented vector indexing. Moved Beeman method calls inside stepForward().

--- Simulation.py
+++ Simulation.py
@@ -2,7 +2,7 @@
     N-body simulation class object.
     Author: Alexander S. Wheaton
     Date: 29 March 2018
-    Updated: 2 April 2018
+    Updated: 4 April 2018
 """
 
 import matplotlib.pyplot
@@ -29,6 +29,13 @@ class Simulation(object):
     
     def stepForward(self):
         
+        for n in range(len(self.system)):
+            self.system[n].beemanPosition(self.timestep)
+        for n in range(len(self.system)):
+            self.system[n].beemanAcceleration()
+        for n in range(len(self.system)):
+            self.system[n].beemanVelocity(self.timestep)
+
         # Updates the position of every body in the system.
         for n in range(len(self.system)):
             self.system[n].stepForward()
@@ -42,17 +49,10 @@ class Simulation(object):
     # Updates the position of each patch with the new simulation data by calling Beeman algorithm functions and stepping the simulation forward.
     def animate(self, i):
         
-        for n in range(len(self.system)):
-            self.system[n].beemanPosition(self.timestep)
-        for n in range(len(self.system)):
-            self.system[n].beemanAcceleration()
-        for n in range(len(self.system)):
-            self.system[n].beemanVelocity(self.timestep)
-        
         self.stepForward()
         
         for n in range(len(self.system)):
-            self.patches[n].center = self.system[n].pos
+            self.patches[n].center = (self.system[n].pos[0], self.system[n].pos[1])
         return(self.patches)
 
     def display(self):
@@ -61,13 +61,17 @@ class Simulation(object):
         figure = matplotlib.pyplot.figure()
         axes = matplotlib.pyplot.axes()
         axes.axis('scaled')
+        axes.set_xlim(-float(1e6), float(1e6))
+        axes.set_ylim(-float(1e6), float(1e6))
+        axes.set_xlabel('x-coordinate (m)')
+        axes.set_ylabel('y-coordinate (m)')
 
         # Creates a list of circles to be plotted by pyplot.
         self.patches = []
 
         for n in range(len(self.system)):
-            self.patches.append(matplotlib.pyplot.Circle(self.system[n].pos.list(), self.system[n].radius, color = self.system[n].colour, animated = True))
-        
+            #self.patches.append(matplotlib.pyplot.Circle((500, 500), 100000, color = 'g', animated = True))
+            self.patches.append(matplotlib.pyplot.Circle((self.system[n].pos[0], self.system[n].pos[1]), self.system[n].radius, color = self.system[n].colour, animated = True))
         for n in range(0, len(self.patches)):
             axes.add_patch(self.patches[n])
         
