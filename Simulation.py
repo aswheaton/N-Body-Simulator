7378435b9b8bac5d4e9f4b0119b7355da4d4348e
Alex Wheaton <34541330+aswheaton@users.noreply.github.com>
2018-04-05 19:28:31 +0100

    Fixed strange vector object behaviour.

--- Simulation.py
+++ Simulation.py
@@ -2,7 +2,7 @@
     N-body simulation class object.
     Author: Alexander S. Wheaton
     Date: 29 March 2018
-    Updated: 4 April 2018
+    Updated: 5 April 2018
 """
 
 import matplotlib.pyplot
@@ -23,16 +23,16 @@ class Simulation(object):
 
         for line in datafile.readlines():
             tokens = line.split(", ")
-            self.system.append(Body(tokens[0], tokens[1], tokens[2], tokens[3], tokens[4], tokens[5], tokens[6]))
+            self.system.append(Body(str(tokens[0]), float(tokens[1]), float(tokens[2]), str(tokens[3]), [float(i) for i in tokens[4].split(",")], [float(i) for i in tokens[5].split(",")], [float(i) for i in tokens[6].split(",")]))
         
         self.timestep = timestep
-    
+
     def stepForward(self):
         
         for n in range(len(self.system)):
             self.system[n].beemanPosition(self.timestep)
         for n in range(len(self.system)):
-            self.system[n].beemanAcceleration()
+            self.system[n].beemanAcceleration(self.system)
         for n in range(len(self.system)):
             self.system[n].beemanVelocity(self.timestep)
 
@@ -61,8 +61,8 @@ class Simulation(object):
         figure = matplotlib.pyplot.figure()
         axes = matplotlib.pyplot.axes()
         axes.axis('scaled')
-        axes.set_xlim(-float(1e6), float(1e6))
-        axes.set_ylim(-float(1e6), float(1e6))
+        axes.set_xlim(-float(4e11), float(4e11))
+        axes.set_ylim(-float(4e11), float(4e11))
         axes.set_xlabel('x-coordinate (m)')
         axes.set_ylabel('y-coordinate (m)')
 
@@ -70,13 +70,12 @@ class Simulation(object):
         self.patches = []
 
         for n in range(len(self.system)):
-            #self.patches.append(matplotlib.pyplot.Circle((500, 500), 100000, color = 'g', animated = True))
-            self.patches.append(matplotlib.pyplot.Circle((self.system[n].pos[0], self.system[n].pos[1]), self.system[n].radius, color = self.system[n].colour, animated = True))
+            self.patches.append(matplotlib.pyplot.Circle((self.system[n].pos[0], self.system[n].pos[1]), 100.0 * self.system[n].radius, color = self.system[n].colour, animated = True))
         for n in range(0, len(self.patches)):
             axes.add_patch(self.patches[n])
         
         # Animates the plot.
-        animation = FuncAnimation(figure, self.animate, init_func = self.init, frames = 1, repeat = True, interval = 0.1, blit = True)
+        animation = FuncAnimation(figure, self.animate, init_func = self.init, frames = 1000, repeat = True, interval = 0.1, blit = True)
         
         # Show the plot.
         matplotlib.pyplot.show()
