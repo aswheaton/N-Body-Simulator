c95832ae8dbb9e3f1ffad51a8eb22f0a2b1c0698
Alex Wheaton <34541330+aswheaton@users.noreply.github.com>
2018-04-06 23:10:16 +0100

    Added (but commented out) some input options.

--- nBodySimulation.py
+++ nBodySimulation.py
@@ -2,11 +2,20 @@
     Test code for n-body simulation class object.
     Author: Alexander S. Wheaton
     Date: 29 March 2018
-    Updated: 1 April 2018
+    Updated: 7 April 2018
 """
 
 from Simulation import Simulation
 
-simulation = Simulation("sol.txt", 1000) #input("Specify a datafile or use a preset: "))
+def main():
 
-simulation.display()
+    while raw_input("Run a simulation (r) or quit (q): ") != "q":
+    
+        filename = "sol.txt" # input("Specify a preset n-body system file: ")
+        timestep = 24 * 3600 # input("specify a timestep (s): ")
+        maxIterations = 100000 # int(input("To how many iterations should the simulation be run: "))
+        simulation = Simulation(filename, timestep, maxIterations)
+
+        simulation.display()
+        
+main()
