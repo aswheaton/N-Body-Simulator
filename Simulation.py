ef3023d65b94f8b56c0f34a4293c7d27f0502f37
Alex Wheaton <34541330+aswheaton@users.noreply.github.com>
2018-04-06 18:24:44 +0100

    Added an energy export method.

--- Simulation.py
+++ Simulation.py
@@ -9,7 +9,6 @@ import matplotlib.pyplot
 
 from matplotlib.animation import FuncAnimation
 
-from Vector import Vector
 from Body import Body
 
 class Simulation(object):
@@ -26,6 +25,8 @@ class Simulation(object):
             self.system.append(Body(str(tokens[0]), float(tokens[1]), float(tokens[2]), str(tokens[3]), [float(i) for i in tokens[4].split(",")], [float(i) for i in tokens[5].split(",")], [float(i) for i in tokens[6].split(",")]))
         
         self.timestep = timestep
+        
+        datafile.close()
 
     def stepForward(self):
         
@@ -38,14 +39,26 @@ class Simulation(object):
 
         # Updates the position of every body in the system.
         for n in range(len(self.system)):
-            self.system[n].stepForward()
+            self.system[n].stepForward(self.timestep)
+        
+    def exportEnergy(self):
+        
+        datafile = open("energy.txt", "w")
+        
+        totalKineticEnergy = 0.0
+        totalPotentialEnergy = 0.0
+        
+        for n in range(len(system)):
+            totalKineticEnergy = totalKineticEnergy + self.system[n].getKineticEnergy()
+            totalPotentialEnergy = totalPotentialEnergy + self.system[n].getGravitationalEnergy(self.system)
+            
+        totalPotentialEnergy = totalPotentialEnergy / 2.0
+        
+        datafile.write(str(elapsedTime) + ", " + str(totalKineticEnergy) + ", " + str(totalPotentialEnergy) + ", " + str(totalKineticEnergy + totalPotentialEnergy) + "\n")
+        
+        datafile.close()
     
-    # This block updates and animates the simulation by calling the stepForward() method and using FuncAnimation.    
-    
-    # Initialiser for the animator.
-    def init(self):
-        return(self.patches)
-    
+    # This block updates and animates the simulation by calling the stepForward() method and using FuncAnimation.      
     # Updates the position of each patch with the new simulation data by calling Beeman algorithm functions and stepping the simulation forward.
     def animate(self, i):
         
@@ -54,6 +67,8 @@ class Simulation(object):
         for n in range(len(self.system)):
             self.patches[n].center = (self.system[n].pos[0], self.system[n].pos[1])
         return(self.patches)
+        
+        self.exportEnergy(self.timestep * i)
 
     def display(self):
         
@@ -61,8 +76,8 @@ class Simulation(object):
         figure = matplotlib.pyplot.figure()
         axes = matplotlib.pyplot.axes()
         axes.axis('scaled')
-        axes.set_xlim(-float(4e11), float(4e11))
-        axes.set_ylim(-float(4e11), float(4e11))
+        axes.set_xlim(-float(3e11), float(3e11))
+        axes.set_ylim(-float(3e11), float(3e11))
         axes.set_xlabel('x-coordinate (m)')
         axes.set_ylabel('y-coordinate (m)')
 
@@ -70,12 +85,15 @@ class Simulation(object):
         self.patches = []
 
         for n in range(len(self.system)):
-            self.patches.append(matplotlib.pyplot.Circle((self.system[n].pos[0], self.system[n].pos[1]), 100.0 * self.system[n].radius, color = self.system[n].colour, animated = True))
+            if self.system[n].name == "Sol":
+                self.patches.append(matplotlib.pyplot.Circle((self.system[n].pos[0], self.system[n].pos[1]), 10.0 * self.system[n].radius, color = self.system[n].colour, animated = True))
+            else:
+                self.patches.append(matplotlib.pyplot.Circle((self.system[n].pos[0], self.system[n].pos[1]), 1000.0 * self.system[n].radius, color = self.system[n].colour, animated = True))
         for n in range(0, len(self.patches)):
             axes.add_patch(self.patches[n])
         
         # Animates the plot.
-        animation = FuncAnimation(figure, self.animate, init_func = self.init, frames = 1000, repeat = True, interval = 0.1, blit = True)
+        animation = FuncAnimation(figure, self.animate, frames = 1, repeat = True, interval = 10, blit = True)
         
         # Show the plot.
         matplotlib.pyplot.show()
